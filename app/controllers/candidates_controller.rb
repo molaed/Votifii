52ed1e2edcfac1017cc18ecf16bb78c57c0af2c3
nya33 <nya33@sfu.ca>
2023-10-27 18:41:56 -0700

    update main again

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,8 +4,14 @@ class CandidatesController < ApplicationController
         @candidate = @election.candidates.create(candidate_params)
         redirect_to election_path(@election)
         end
+    def destroy
+        @election = Election.find(params[:election_id])
+        @candidate = @election.candidates.find(params[:id])
+        @candidate.destroy
+        redirect_to election_path(@election), status: :see_other
+    end
 
-    private
+private
     def candidate_params
         params.require(:candidate).permit(:name, :description)
     end
