14fec7e83eb27e43714d5b70ca32600d59199edd
Sadra Manouchehrifar <sadra.manouchehri@gmail.com>
2023-10-31 21:23:33 -0700

    Background and styling

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_043233) do
   create_table "candidates", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "voteCount", default: 0
+    t.integer "voteCount"
     t.bigint "election_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,18 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_043233) do
     t.string "status"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.bigint "candidate_id", null: false
-    t.text "body"
-    t.bigint "election_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title"
-    t.index ["candidate_id"], name: "index_posts_on_candidate_id"
-    t.index ["election_id"], name: "index_posts_on_election_id"
-  end
-
   add_foreign_key "candidates", "elections"
-  add_foreign_key "posts", "candidates"
-  add_foreign_key "posts", "elections"
 end
