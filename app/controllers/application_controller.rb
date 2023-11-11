175d46dc77776afa5a45da2eeb1412d056bd9fd0
Nathan Young <116300272+molaed@users.noreply.github.com>
2023-11-10 22:57:26 -0800

    Added api call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    require 'rest-client'
+    require 'json'
+
+    before_action :render_pic
+
+    def render_pic
+        response = RestClient.get "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+        
+        hash = JSON.parse(response) #parse creates a hash with {key: message, value, }
+
+        @image = hash["url"]
+    end
 end
