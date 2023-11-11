134e5fbb4dc32e9d939c1d6adfa83846982b103f
Arya-Fgmain <aryafg2004@gmail.com>
2023-11-10 23:34:48 -0800

    changed candidate model to show image attachment

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,6 +2,8 @@ class Candidate < ApplicationRecord
   belongs_to :election, optional: true
   after_initialize :default_values
 
+  has_one_attached: :profile_image # profile_images are attached to the candidate object
+
 private
   def default_values
     self.voteCount ||= 0
