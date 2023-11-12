7e34875601e64a16d65f55d40c019f9f58831e57
gnc3 <gnc3@sfu.ca>
2023-11-11 17:58:46 -0800

    navbar added and changed the text format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
     require 'rest-client'
     require 'json'
-
+    
     before_action :render_pic
 
     def render_pic
-        response = RestClient.get "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+        #response = RestClient.get "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
         
-        hash = JSON.parse(response) #parse creates a hash with {key: message, value, }
+        #hash = JSON.parse(response) #parse creates a hash with {key: message, value, }
 
-        @image = hash["url"]
+        #@image = hash["url"]
     end
+    
 end
