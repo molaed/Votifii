c945ed195753246a1f28509f0391a56496909cb9
Nathan Young <116300272+molaed@users.noreply.github.com>
2023-11-11 21:03:54 -0800

    Updated API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
     require 'rest-client'
     require 'json'
-    #Breaks everything with 'too many requests'. Maybe because of demo?
+
     #before_action :render_pic
 
-    #def render_pic
-    #    response = RestClient.get "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    def render_pic
+       response = RestClient.get "https://picsum.photos/200"
         
-    #    hash = JSON.parse(response) #parse creates a hash with {key: message, value, }
+       hash = JSON.parse(response)
 
-    #    @image = hash["10"]
-    #end
+       @image = hash
+    end
 end
